6ee70acca4021ac713e5b58736ec743a64e45580
Randy-Coding <164274204+Randy-Coding@users.noreply.github.com>
2024-03-21 11:42:34 -0400

    updated script_to_exe

--- script_to_exe.py
+++ script_to_exe.py
@@ -11,7 +11,15 @@ def create_executable(py_file_path):
         subprocess.check_call([sys.executable, "-m", "PyInstaller", "--onefile",
                                "--distpath", ".", py_file_path])
         print(f"Executable created successfully for {py_file_path} in the current directory")
+
+        # Construct the .spec file path to delete
+        spec_file_path = os.path.splitext(py_file_path)[0] + '.spec'
         
+        # Check if the .spec file exists and delete it
+        if os.path.exists(spec_file_path):
+            os.remove(spec_file_path)
+            print(f"Deleted {spec_file_path}")
+
     except subprocess.CalledProcessError as e:
         print(f"Error during the creation of the executable: {e}")
     except Exception as e:

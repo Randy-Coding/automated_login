e0fec23c0d3c4f37ea641914da174adf9e1cc982
Randy-Coding <164274204+Randy-Coding@users.noreply.github.com>
2024-03-25 22:35:57 -0400

    Comments
    Added some comments and updated the send_keys

--- automate.py
+++ automate.py
@@ -8,10 +8,12 @@ from pywinauto import Desktop
 
 
 def is_window_present():
+    # This will go through all currently open windows and will locate the one that contains the title "1Password"
+    # If it finds it, it will return true
     desktop = Desktop(backend="win32")
     window = desktop.window(title="1Password", visible_only=True)
 
-    if window.exists(timeout=5):
+    if window.exists(timeout=2):
         return True
     return False
 
@@ -21,8 +23,9 @@ def sign_in_with_1password(op_path, password):
     # Start the 1Password CLI sign-in process as a subprocess
     subprocess.Popen([op_path, "signin"])
 
+    # Sends the password if the window is found, and hits enter.
     if is_window_present():
-        send_keys(password)
+        send_keys(f"{password}{{ENTER}}")
 
 
 # Assuming a default op_path. Modify as needed.
